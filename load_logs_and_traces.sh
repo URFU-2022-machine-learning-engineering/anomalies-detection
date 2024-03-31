bdd51fba120f990332b562a81368032be701817c
Vladimir Katin <katin.v.v@gmail.com>
2024-03-31 03:18:22 +0300

    update script

--- load_logs_and_traces.sh
+++ load_logs_and_traces.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-# cd /path/to/your/project  # Ensure we're in the right directory
+cd "$HOME/anomalies-detection" || exit 1  # Ensure we're in the right directory
 
-poetry update
-dvc pull
+git pull
+poetry run dvc pull
 sleep 5
 poetry run python load_traces/main.py
 sleep 5
-dvc add data/
+poetry run dvc add data/
 git add data.dvc
 git commit -m "Daily backup of data"
 git push
-dvc push
+poetry run dvc push
+
